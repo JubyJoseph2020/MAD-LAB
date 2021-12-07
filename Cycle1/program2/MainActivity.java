8942f9eb881329049aa8c577d9fd5f9e565ce42b
JubyJoseph2020 <74653069+JubyJoseph2020@users.noreply.github.com>
2021-12-07 11:16:33 +0530

    Add files via upload

---
+++ Cycle1/program2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.sjcet.contentchange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.tv);
+    }
+
+
+
+    public void change(View view) {
+        tv.setText("jilu");
+    }
+}
